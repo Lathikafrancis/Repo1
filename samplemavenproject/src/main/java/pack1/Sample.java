db3cd265101cfe596412112b7b73116348190cce
LENOVO <LENOVO@DESKTOP-3P7B85N>
2025-03-19 12:36:12 +0530

    First commit

---
+++ samplemavenproject/src/main/java/pack1/Sample.java
@@ -0,0 +1,15 @@
+package pack1;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sample obj = new Sample();
+		obj.print();
+
+	}
+	public void print()
+	{
+		System.out.println("hello");
+	}
+}
---
+++ samplemavenproject/src/main/java/pack1/Sample2.java
@@ -0,0 +1,18 @@
+package pack1;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(Sample2.add(1,2));
+	}
+	
+	public static int add(int a, int b)
+	{
+	int c = a+b;
+	{
+		return c;
+	}
+}
+}
+
